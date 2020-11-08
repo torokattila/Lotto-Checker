3396d0de04019e85fdd94dd912a99f746ce33ef7
Török Attila <torcsiattila93@gmail.com>
2020-11-08 21:07:49 +0100

    refactor(index): modified headers

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ const app = express();
 app.use(cors());
 // app.options('*', cors());
 
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-//     if (req.method === 'OPTIONS') {
-//         res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-//         return res.status(200).json({});
-//     }
-//     next();
-// });
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Content-Length, X-Requested-With");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    
+    if (req.method === 'OPTIONS') {
+        res.send(200);
+    } else {
+        next();
+    }
+});
 
 app.use(express.static(__dirname + '/static'));
 app.use(express.static('controller'));
